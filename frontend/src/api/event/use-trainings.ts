b95afdc949623bc0c5ca49e14408772dcfc93038
alanleungcn <39870851+alanleungcn@users.noreply.github.com>
2024-08-24 17:06:56 +0800

    feat(frontend): dummy admin manage trainings

---
+++ frontend/src/api/event/use-trainings.ts
@@ -0,0 +1,15 @@
+// // import { apiClient } from "@/api";
+// // import { useQuery } from "@tanstack/react-query";
+// // import { add } from "date-fns";
+
+// export interface Training {
+//   trainingId: number;
+//   trainingName: string;
+//   trainingDescription: string;
+//   // training
+//   // pending
+// }
+
+// export function useTrainings() {
+//   return {};
+// }
--- frontend/src/components/admin/manage-trainings.tsx
+++ frontend/src/components/admin/manage-trainings.tsx
@@ -1,3 +1,37 @@
+import { Input } from "../ui/input";
+import { TrainingCard } from "../ui/trainingcard";
+// import { useTrainings } from "@/api/event/use-trainings";
+
 export const ManageTrainings = () => {
-  return <div>Manage Trainings</div>;
+  // const {  } = useTrainings();
+
+  return (
+    <div className="flex justify-center">
+      <div className="p-8 max-w-[1000px] w-full flex flex-col gap-8">
+        <h1 className="text-2xl font-bold">Manage Events</h1>
+        <div className="flex gap-8">
+          <Input placeholder="Search" />
+        </div>
+
+        <div>
+          {/* {data?.map((e) => (
+            <EventCard
+              key={e.eventId}
+              event={e}
+              buttonText="Edit"
+              buttonAction={() => {}}
+              layout={layout}
+            />
+          ))} */}
+          <TrainingCard
+            description="asdfasdf"
+            tags={["asdffsdf", "afsdf"]}
+            title="asdfasdf"
+            videoSrc="https://www.youtube.com/embed/xDPL-_Op87o"
+            key={1}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };

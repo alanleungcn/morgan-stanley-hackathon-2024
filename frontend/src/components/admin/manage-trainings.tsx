c3ce6656f255d08ebe045d93b0609ea26b01e6b4
Kalyanlimbu <kalyanlimbu135@gmail.com>
2024-08-24 17:47:17 +0800

    fix imports

--- frontend/src/components/admin/manage-trainings.tsx
+++ frontend/src/components/admin/manage-trainings.tsx
@@ -1,16 +1,16 @@
-import { Input } from "../ui/input";
-import { TrainingCard } from "../ui/trainingcard";
+import { Input } from '../ui/input'
+import { TrainingCard } from '../training/trainingcard'
 // import { useTrainings } from "@/api/event/use-trainings";
 
 export const ManageTrainings = () => {
   // const {  } = useTrainings();
 
   return (
-    <div className="flex justify-center">
-      <div className="p-8 max-w-[1000px] w-full flex flex-col gap-8">
-        <h1 className="text-2xl font-bold">Manage Events</h1>
-        <div className="flex gap-8">
-          <Input placeholder="Search" />
+    <div className='flex justify-center'>
+      <div className='p-8 max-w-[1000px] w-full flex flex-col gap-8'>
+        <h1 className='text-2xl font-bold'>Manage Events</h1>
+        <div className='flex gap-8'>
+          <Input placeholder='Search' />
         </div>
 
         <div>
@@ -24,14 +24,14 @@ export const ManageTrainings = () => {
             />
           ))} */}
           <TrainingCard
-            description="asdfasdf"
-            tags={["asdffsdf", "afsdf"]}
-            title="asdfasdf"
-            videoSrc="https://www.youtube.com/embed/xDPL-_Op87o"
+            description='asdfasdf'
+            tags={['asdffsdf', 'afsdf']}
+            title='asdfasdf'
+            videoSrc='https://www.youtube.com/embed/xDPL-_Op87o'
             key={1}
           />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
--- frontend/src/components/training/trainingcard.tsx
+++ frontend/src/components/training/trainingcard.tsx
@@ -1,54 +1,61 @@
-import { useState } from 'react';
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import TrainingModal from '@/components/ui/trainingmodel';
+import { useState } from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import TrainingModal from '@/components/training/trainingmodel'
 
 type TrainingCardProps = {
-  title: string;
-  description: string;
-  videoSrc: string;
-  tags: string[];
-};
+  title: string
+  description: string
+  videoSrc: string
+  tags: string[]
+}
 
-export const TrainingCard = ({ title, description, videoSrc, tags }: TrainingCardProps) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+export const TrainingCard = ({
+  title,
+  description,
+  videoSrc,
+  tags,
+}: TrainingCardProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
   const handleCardClick = () => {
-    setIsModalOpen(true);
-  };
+    setIsModalOpen(true)
+  }
 
   const handleModalClose = () => {
-    setIsModalOpen(false);
-  };
+    setIsModalOpen(false)
+  }
 
   return (
     <>
       <Card
-        className="flex flex-col overflow-hidden transition-shadow duration-300 bg-white rounded-lg shadow-lg cursor-pointer lg:flex-row hover:shadow-xl"
+        className='flex flex-col overflow-hidden transition-shadow duration-300 bg-white rounded-lg shadow-lg cursor-pointer lg:flex-row hover:shadow-xl'
         onClick={handleCardClick}
       >
-        <CardHeader className="p-0 rounded-t-lg lg:w-1/3 lg:rounded-t-none lg:rounded-l-lg">
+        <CardHeader className='p-0 rounded-t-lg lg:w-1/3 lg:rounded-t-none lg:rounded-l-lg'>
           <iframe
             src={videoSrc}
-            className="object-cover w-full h-48 rounded-t-lg lg:h-full lg:rounded-t-none lg:rounded-l-lg"
+            className='object-cover w-full h-48 rounded-t-lg lg:h-full lg:rounded-t-none lg:rounded-l-lg'
             title={title}
             allowFullScreen
-            loading="lazy"
+            loading='lazy'
           />
         </CardHeader>
-        <div className="flex flex-col lg:w-2/3">
-          <CardHeader className="p-4 bg-gray-100 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg">
-            <CardTitle className="text-xl font-semibold text-gray-800">{title}</CardTitle>
+        <div className='flex flex-col lg:w-2/3'>
+          <CardHeader className='p-4 bg-gray-100 rounded-t-lg lg:rounded-t-none lg:rounded-r-lg'>
+            <CardTitle className='text-xl font-semibold text-gray-800'>
+              {title}
+            </CardTitle>
           </CardHeader>
-          <CardContent className="flex-1 p-4">
-            <p className="h-24 overflow-hidden text-gray-700 text-ellipsis">{description}</p>
-            <div className="flex flex-wrap gap-2 mt-4">
+          <CardContent className='flex-1 p-4'>
+            <p className='h-24 overflow-hidden text-gray-700 text-ellipsis'>
+              {description}
+            </p>
+            <div className='flex flex-wrap gap-2 mt-4'>
               {tags.map((tag, index) => (
-                <span key={index} className="px-3 py-1 text-yellow-800 bg-yellow-200 rounded-full">
+                <span
+                  key={index}
+                  className='px-3 py-1 text-yellow-800 bg-yellow-200 rounded-full'
+                >
                   {tag}
                 </span>
               ))}
@@ -66,5 +73,5 @@ export const TrainingCard = ({ title, description, videoSrc, tags }: TrainingCar
         tags={tags}
       />
     </>
-  );
-};
+  )
+}

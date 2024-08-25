5b454821e73a3356122df2b8085820e044e77ec5
alanleungcn <39870851+alanleungcn@users.noreply.github.com>
2024-08-25 18:30:54 +0800

    fix(frontend): admin stat page title

--- frontend/src/components/admin/wellbeing.tsx
+++ frontend/src/components/admin/wellbeing.tsx
@@ -5,40 +5,36 @@ import { VegaLite } from "react-vega";
 export const Wellbeing = () => {
   const { data, isSuccess } = useCharts();
 
-  console.log(data?.reviewCharts);
+  return isSuccess ? (
+    <div className="flex justify-center">
+      <div className="flex w-full flex-col gap-5 p-8">
+        <h1 className="text-4xl font-bold">Statistics</h1>
 
-  return (
-    isSuccess && (
-      <div className="flex justify-center">
-        <div className="flex w-full flex-col gap-5 p-8">
-          <h1 className="text-4xl font-bold">Wellbeing Statistics</h1>
-
-          <div className="space-y-4">
-            <div className="flex space-x-4">
-              <div className="rounded-md bg-primary/30 p-6 shadow">
-                <div className="font-mono text-xl font-black">150</div>
-                Emails Sent (+18%)
-              </div>
-              <div className="rounded-md bg-primary/30 p-6 shadow">
-                <div className="font-mono text-xl font-black">90</div>
-                Event Reviews (+12%)
-              </div>
-              <div className="rounded-md bg-primary/30 p-6 shadow">
-                <div className="font-mono text-xl font-black">130</div>
-                Wellbeing Received (+15%)
-              </div>
+        <div className="space-y-4">
+          <div className="flex space-x-4">
+            <div className="rounded-md bg-primary/30 p-6 shadow">
+              <div className="font-mono text-xl font-black">150</div>
+              Emails Sent (+18%)
             </div>
-
-            <h1 className="text-2xl font-bold">Event Review</h1>
-            <VegaLite spec={data.reviewCharts} />
-            <VegaLite spec={data.eventTypesChart} />
-
-            <h1 className="text-2xl font-bold">Wellbeing Review</h1>
-            <VegaLite spec={data.wellbeingDistributionChart} />
-            <VegaLite spec={data.wellbeingCharts} />
+            <div className="rounded-md bg-primary/30 p-6 shadow">
+              <div className="font-mono text-xl font-black">90</div>
+              Event Reviews (+12%)
+            </div>
+            <div className="rounded-md bg-primary/30 p-6 shadow">
+              <div className="font-mono text-xl font-black">130</div>
+              Wellbeing Received (+15%)
+            </div>
           </div>
+
+          <h1 className="text-2xl font-bold">Event Review</h1>
+          <VegaLite spec={data.reviewCharts} />
+          <VegaLite spec={data.eventTypesChart} />
+
+          <h1 className="text-2xl font-bold">Wellbeing Review</h1>
+          <VegaLite spec={data.wellbeingDistributionChart} />
+          <VegaLite spec={data.wellbeingCharts} />
         </div>
       </div>
-    )
-  );
+    </div>
+  ) : null;
 };
